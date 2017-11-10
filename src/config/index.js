4ba9b3eafbc3ddf8dcd4cf69aa699d6703bcc459
elzup <guild0105@gmail.com>
2017-11-10 10:46:40 +0900

    Add auth

--- src/config/index.js
+++ src/config/index.js
@@ -4,12 +4,16 @@ const {
 	NODE_ENV,
 	REACT_APP_GITHUB_AUTH_CLIENT_ID,
 	REACT_APP_GITHUB_AUTH_CLIENT_SECRET,
+	PUBLIC_URL,
 } = process.env
+
+console.log(process.env)
 
 if (
 	!REACT_APP_GITHUB_AUTH_CLIENT_ID ||
 	!REACT_APP_GITHUB_AUTH_CLIENT_SECRET ||
-	!NODE_ENV
+	!NODE_ENV ||
+	PUBLIC_URL === undefined
 ) {
 	console.error(process.env)
 	throw new Error('Configuration not enouth. you need setup environments.')
@@ -29,12 +33,16 @@ const isDev = NODE_ENV === 'development'
 const configDevelopment = {}
 const configProduction = {}
 
+const clientId = REACT_APP_GITHUB_AUTH_CLIENT_ID
+const clientSecret = REACT_APP_GITHUB_AUTH_CLIENT_SECRET
+const authUrl = `https://github.com/login/oauth/authorize?client_id=${clientId}`
+
 const config: Config = {
 	isDev,
 	github: {
-		authUrl: 'https://github.com/login/oauth/authorize?client_id=',
-		clientId: REACT_APP_GITHUB_AUTH_CLIENT_ID,
-		clientSecret: REACT_APP_GITHUB_AUTH_CLIENT_SECRET,
+		authUrl,
+		clientId,
+		clientSecret,
 	},
 	...(isDev ? configDevelopment : configProduction),
 }
--- src/containers/App/index.js
+++ src/containers/App/index.js
@@ -1,27 +1,23 @@
 // @flow
 import React from 'react'
 import config from '../../config'
-import { BrowserRouter as Router, Route, Link } from 'react-router-dom'
+import { BrowserRouter as Router, Route } from 'react-router-dom'
 
 // import Home from '../../components/Home'
 
 const IssuePage = ({ match }) => <div>IssuePage {match.params.id}</div>
 
 const Home = () => {
-	return (
-		<Link to={config.github.authUrl + config.github.clientId}>
-			Github Login
-		</Link>
-	)
+	return <a href={config.github.authUrl}>Github Login</a>
 }
 
 const App = () => (
 	<Router>
 		<div>
 			<Route exact path="/" component={Home} />
-			<Route path="/issue/:id" component={IssuePage} />
+			<Route path="/authed/" component={IssuePage} />
+			<Route path="/authed/:id" component={IssuePage} />
 		</div>
 	</Router>
 )
-
 export default App
